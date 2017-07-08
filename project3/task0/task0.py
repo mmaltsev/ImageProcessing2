393e6154743e8d013b78eb3af292db5247a63596
Alexey Karpov <karpovad@yandex.ru>
2017-07-08 20:01:38 +0200

    cleaned files

--- project3/task0/task0.py
+++ project3/task0/task0.py
@@ -1,27 +1,27 @@
 
 # coding: utf-8
 
-# In[38]:
+# In[1]:
 
 import os
 import numpy as np
 import scipy.misc as msc
 
 
-# In[29]:
+# In[2]:
 
 def read_data(folder):
     files = os.listdir(folder)
     return files
 
 
-# In[30]:
+# In[3]:
 
-train_folder = 'uiucTrain'
+train_folder = '../uiucTrain'
 train_names = read_data(train_folder)
 
 
-# In[39]:
+# In[4]:
 
 def read_and_center(folder, names):
   X = np.stack([msc.imread(folder + '/' + f).flatten() for f in names], axis=0)
@@ -30,7 +30,7 @@ def read_and_center(folder, names):
   return X_center, mean
 
 
-# In[54]:
+# In[5]:
 
 train_images, _ = read_and_center(train_folder, train_names)
 train_images = [(image, 1 if 'Pos' in train_names[index] else -1) for index, image in enumerate(train_images)]
