aed165f250f7f5bdf419c751f0037ddd86cf928c
Alexey Karpov <karpovad@yandex.ru>
2017-07-08 20:19:01 +0200

    read shape of images

--- project3/task0/task0.py
+++ project3/task0/task0.py
@@ -1,38 +1,40 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[9]:
 
 import os
 import numpy as np
 import scipy.misc as msc
 
 
-# In[2]:
+# In[10]:
 
 def read_data(folder):
     files = os.listdir(folder)
     return files
 
 
-# In[3]:
+# In[11]:
 
 train_folder = '../uiucTrain'
 train_names = read_data(train_folder)
 
 
-# In[4]:
+# In[46]:
 
 def read_and_center(folder, names):
-  X = np.stack([msc.imread(folder + '/' + f).flatten() for f in names], axis=0)
-  mean = np.mean(X, axis=0)
-  X_center = (X - mean).T
-  return X_center, mean
+    X = np.stack([msc.imread(folder + '/' + f) for f in names], axis=0)
+    shape = X.shape[1:]
+    X = [image.flatten() for image in X]
+    mean = np.mean(X, axis=0)
+    X_center = (X - mean)
+    return X_center, mean, shape
 
 
-# In[5]:
+# In[47]:
 
-train_images, _ = read_and_center(train_folder, train_names)
+train_images, _, shape = read_and_center(train_folder, train_names)
 train_images = [(image, 1 if 'Pos' in train_names[index] else -1) for index, image in enumerate(train_images)]
 
 
